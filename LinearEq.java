c480a4e0c1a8b9f3d5ce5c06e72a0db76632f055
Rotemkal <154257850+Rotemkal@users.noreply.github.com>
2023-12-18 19:31:03 +0200

    HW1.Code

--- LinearEq.java
+++ LinearEq.java
@@ -1,5 +1,6 @@
 public class LinearEq {
 	public static void main(String[] args) {
+		//solves linear equations of the form 𝑎 ⋅ 𝑥 + 𝑏 = 𝑐
 		double a = Integer.parseInt(args[0]);
 		double b = Integer.parseInt(args[1]);
 		double c = Integer.parseInt(args[2]);

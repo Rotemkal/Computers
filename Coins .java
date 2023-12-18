9fe3cde5dec221959057d2fe3466dffc3bd789a6
Rotemkal <154257850+Rotemkal@users.noreply.github.com>
2023-12-18 19:29:40 +0200

    Add files via upload

---
+++ Coins .java
@@ -0,0 +1,10 @@
+public class Coins {
+	public static void main(String[] args) {
+	   // gets a number of cents as a command-line argument and rints how to represent the 
+	   //quantity using as many quarters as possible plus the remainder in cents
+		int a= Integer.parseInt(args[0]);
+		int b= a/25;
+		int c= a%25;
+		System.out.println("use "+b+" quarters and "+c+" cents");
+}
+}

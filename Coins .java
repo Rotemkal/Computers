67ec26b922a54ad2d3b9ab66e722b89e6b831a0a
רותם קליר <rotemk@h-MacBook-Pro-sl-rwtm.local>
2023-12-19 22:11:39 +0200

    fix print

--- Coins .java
+++ Coins .java
@@ -1,6 +1,6 @@
 public class Coins {
 	public static void main(String[] args) {
-	   // gets a number of cents as a command-line argument and rints how to represent the 
+	   // gets a number of cents as a command-line argument and prints how to represent the 
 	   //quantity using as many quarters as possible plus the remainder in cents
 		int a= Integer.parseInt(args[0]);
 		int b= a/25;
